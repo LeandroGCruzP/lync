8dc45b6156723d8684a91ddceaf7a97e212fc061
Leandro Cruz <leandro.programmer@hotmail.com>
2025-11-19 08:52:31 -0300

    feat(error-handler): implement centralized error handling for the application

---
+++ src/http/error-handler.ts
@@ -0,0 +1,59 @@
+import { FastifyInstance } from 'fastify'
+import { hasZodFastifySchemaValidationErrors } from 'fastify-type-provider-zod'
+import { ZodError } from 'zod'
+
+import { BadRequestError } from './routes/_errors/bad-request-error'
+import { UnauthorizedError } from './routes/_errors/unauthorized-error'
+
+type FastifyErrorHandler = FastifyInstance['errorHandler']
+
+export const errorHandler: FastifyErrorHandler = (error, _, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    const formattedErrors: Record<string, string[]> = {}
+
+    for (const issue of error.validation) {
+      if (issue.instancePath && issue.message) {
+        const fieldName = issue.instancePath.substring(1)
+
+        if (fieldName) {
+          if (!formattedErrors[fieldName]) {
+            formattedErrors[fieldName] = []
+          }
+          formattedErrors[fieldName].push(issue.message)
+        }
+      }
+    }
+
+    return reply.status(400).send({
+      message: 'Validation error.',
+      errors: formattedErrors,
+    })
+  }
+
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      errors: error.flatten().fieldErrors,
+    })
+  }
+
+  if (error instanceof BadRequestError) {
+    return reply.status(400).send({
+      message: error.message,
+    })
+  }
+
+  if (error instanceof UnauthorizedError) {
+    return reply.status(400).send({
+      message: error.message,
+    })
+  }
+
+  console.log(error)
+
+  // send error to some observability platform
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -7,12 +7,15 @@ import {
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from '~/lib/env'
+import { errorHandler } from './error-handler'
 import { routes } from './routes'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.setErrorHandler(errorHandler)
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
@@ -22,5 +25,5 @@ app.register(fastifyCors)
 app.register(routes)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
-  console.log('🚀 HTTP server running!')
+  console.log(`🚀 HTTP server running on port ${env.SERVER_PORT}!`)
 })
